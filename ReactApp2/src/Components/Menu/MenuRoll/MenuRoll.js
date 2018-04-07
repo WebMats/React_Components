b0519998ab279ea1654813e64555db438f6ebd70
OrlandoL10 <lorlando127@gmail.com>
2018-04-06 23:53:19 -0500

    added template for menu display

---
+++ ReactApp2/src/Components/Menu/MenuRoll/MenuRoll.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const menuroll = (props) => {
+
+
+	return (
+
+		<div>
+			<h1>MenuRoll</h1>
+		</div>
+		);
+
+	};
+
+export default menuroll ;
--- ReactApp2/src/containers/Menu/Menu.js
+++ ReactApp2/src/containers/Menu/Menu.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
 import Navbar from '../../Components/UI/Navbar/Navbar';
+import MenuRoll from '../../Components/Menu/MenuRoll/MenuRoll';
 
 import Aux from '../../hoc/Aux/Aux';
-import Logo from '../../assets/images/logomark.svg';
-import MVLogo from '../../assets/images/mv-logo.png';
+import Mark from '../../assets/images/logomark.svg';
+import Logo from '../../assets/images/mv-logo.png';
 
 import Css from './Menu.css';
 
@@ -15,9 +16,16 @@ class Menu extends Component {
 	render() {
 
 		return (
-			<div>
-				<h1>Menu</h1>
+		<Aux>
+			<div className={Css.LogoWrapper}>
+				<img src={Mark} alt=""/>
+				<img src={Logo} alt=""/>
 			</div>
+			<hr className={Css.Line} />
+			<br /> <br /> <br />
+			<Navbar />
+			<MenuRoll />
+		</Aux>
 			);
 	}
 }
